b2467709f509aa18677670745ea2367687dc3b41
Chinilaw TV <zoom.flux@gmail.com>
2019-07-10 09:23:06 +0800

    Initial Commit

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Jules Lee on 10/07/2019.
+//  Copyright © 2019 Jules Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
